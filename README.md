0ec91ebb9298b99a3a15a17d6c1aa316bdc7cb8f
Ayan Saeed <danny4saeed@gmail.com>
2025-10-11 14:55:15 +0100

    Added code for HTML, CSS and JavaScript. The web page now allows users to search for movies, displays currently trending movies on the main page, and also dynamically adjusts the movie posters and titles when the web page is minimised/shrunk by the user

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Movie-Web-App
 A full-stack movie web app built with HTML, CSS, and JavaScript, featuring both front-end UI and backend integration for data handling.
+
+## About this Project 
+As an avid movie enthusiast, who frequently uses IMDb to discover and track films, I wanted to combine my love for movies with my interest in web developement. This project was my earnest attempt at recreating a simplified version of IMDB, allowing users to browse through popular movies, search for specific titles, and view basic movie info. This project aims to improve my web development skills by building a full-stack web application inspired by IMDb's interface.
+
+## Initial Requirements (In order of Priority)
+Before starting, I wanted to highlight and analyse the key requirements that my project must include. From initial observations, the basic functionalities of IMDb are comprised of: 
+- **Search for Specific Movies**. Argueably the most important feature, as it allows users to explore and search for titles.
+- **Browse Movies**. View the currently trending movies on the main page. 
+- **Sign in**. Allows users to create a personal account, in order to track their watchlists etc. 
+- **View detailed movie information**. Including release year, runtime, cast and other relevent information. 
+- **See ratings**.
+### Future / Low-Priority Features
+- **Watchlist**. More of a nice-to-have feature. 
--- javaScript-web-app.js
+++ javaScript-web-app.js
@@ -1,1 +1,83 @@
- 
+/* This is the URL for TMDb's "Discover Movies" API. 
+   Fetches the movies sorted my popularity, in descending order */ 
+const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0cec74559973c77aa1ad5ebabb957d6b&page=1";
+// URL for the movie posters */
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+// URL for searching movies by query text */
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=0cec74559973c77aa1ad5ebabb957d6b&query=";
+
+// References to elements
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+const returnToMain = document.getElementsByClassName("logo")[0];
+
+/* Calls the returnMovies function,
+   so popular movies will be displayed on page load */
+returnMovies(APILINK);
+function returnMovies(url){
+    // Sends a request to the API URL
+    fetch(url).then(res => res.json())
+    /* Creates a 'h2' heading named "Popular today", 
+       and appends it to main*/
+    .then(function(data){
+        const heading = document.createElement('h2');
+        heading.innerText = "Popular Today";
+        heading.setAttribute('id', 'popular-heading');
+        main.appendChild(heading);
+        /* Array of movies returned by the movie API,
+           and Loops through each movie within the array */
+        console.log(data.results);
+        data.results.forEach(element =>{
+            // Creates a layout for the movie posters titles
+            const div_card = document.createElement('div');
+            div_card.setAttribute('class','card');
+            const div_row = document.createElement('div');
+            div_row.setAttribute('class','row');
+            const div_column = document.createElement('div');
+            div_column.setAttribute('class','column');
+            // Creates an 'img' element for the movie poster 
+            const image = document.createElement('img');
+            image.setAttribute('class','thumbnail');
+            image.setAttribute('id','image');
+            //Creates a 'h3' for the movie title
+            const title = document.createElement('h3');
+            title.setAttribute('id','title');
+            /* Wraps the image in a '<center>' element, as well as setting up
+               the title text and poster source image*/
+            const center = document.createElement('center');
+            title.innerHTML = `${element.title}`;
+            image.src = IMG_PATH + element.poster_path;
+            center.appendChild(image);
+            /* Appends the image and title to the card, card to column, column to row, 
+               and row to the main section */
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+            main.appendChild(div_row);
+        });
+    });
+}
+
+/* Listens for a form submission, i.e. a movie query. If detected,
+   it clears the current movie display and gets the search query, as well as, calling 'returnMovies'
+   to return the movies that fit the query submission. Lastly, it clears the input field */
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+    const searchItem = search.value;
+    if (searchItem){
+        returnMovies(SEARCHAPI + searchItem);
+        search.value = '';
+    }
+})
+
+/* When the logo is clicked, it clears the main section, and returns to the default page,
+   i.e. it reloads popular movies by calling returnMovies(APILINK). Useful if user wants to return to main page, 
+   after a search query */
+returnToMain.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+    returnMovies(APILINK);
+});
